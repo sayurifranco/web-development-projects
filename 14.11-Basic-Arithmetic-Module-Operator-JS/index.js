a910c92d97a68c811b9534e365eb7efd7f7794b1
sayurifranco <lsshiroma@gmail.com>
2023-09-01 13:44:29 +0100

    Examples of arithmetic module operator

--- 14.11-Basic-Arithmetic-Module-Operator-JS/index.js
+++ 14.11-Basic-Arithmetic-Module-Operator-JS/index.js
@@ -1,3 +1,5 @@
 var dogAge = prompt("How old is your dog?");
 var humanAge = ((dogAge - 2)*4)+21;
-alert("Your dog is " + humanAge + "years old in human years.");
+alert("Your dog is " + humanAge + "years old in human years."); 
+
+// Alerta vai ser visto pelo visitante do site. Console log apenas visto pelo dev
