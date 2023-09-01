38194715b300b70225f2fa191046a87bdcbcd98e
sayurifranco <lsshiroma@gmail.com>
2023-09-01 17:28:13 +0100

    exercises of JS part II

---
+++ 14.13-Functions_Part-2/index.js
@@ -0,0 +1,28 @@
+function getMilk(money) {   
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+
+    var numberOfBottles = Math.floor(money / 1.5); 
+    //Math.floor para número inteiro
+
+    console.log("buy " + numberOfBottles + "bottles of milk");
+
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+  }
+
+  getMilk(5); //$1.5 5/1.5 = 3 bottles of milk
