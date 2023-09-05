61e96746328ddd65c9fc14b4e02e3ac15cd13a00
sayurifranco <lsshiroma@gmail.com>
2023-09-05 16:14:23 +0100

    Exercise of Ramdom Number Generation in JS

---
+++ 14.17-Ramdom-Number-Generation/index.js
@@ -0,0 +1,11 @@
+// var n = Math.random(); floating-point 0 up to 1
+// n = n * 6;
+// n = Math.floor(n) + 1;
+// console.log(n);
+
+prompt("What is your name?");
+prompt("What's their name?");
+
+var loveScore = Math.random() * 100; //floating-point 0 up to 1
+loveScore = Math.floor(loveScore) + 1
+alert("Your love score is " + loveScore + "%");
