855f2d00a1bda739328735aced65af6388c9ebc8
sayurifranco <lsshiroma@gmail.com>
2023-09-06 13:51:14 +0100

    examples of combining comparators

---
+++ JavaScript/15.3-Combining-Comparators/index.js
@@ -0,0 +1,34 @@
+prompt("What is your name?");
+prompt("What's their name?");
+
+var loveScore = Math.random() * 100; //floating-point 0 up to 1
+loveScore = Math.floor(loveScore) + 1 //1-100
+
+if (loveScore > 70) {
+    alert("Your love score is " + loveScore + "%." + " You love each other like Kanye loves Kanye.");
+} 
+
+if (loveScore > 30 && loveScore <= 70)
+    alert("Your love score is " + loveScore + "%.");
+
+if (loveScore <= 30) {
+    alert("Your love score is " + loveScore + "%." + " You go together like oil and water.");
+}
+
+var a = 1;
+var b = "1";
+
+typeof(a); //number
+typeof(b); //string
+
+if (a === b) {
+    console.log("yes")
+} else {
+    console.log("no");
+} // no - double match (type and number)
+
+if (a == b) {
+    console.log("yes")
+} else {
+    console.log("no");
+} // yes
