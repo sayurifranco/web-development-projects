114b829e2e0e34de368cfa60f680756a2988996a
sayurifranco <lsshiroma@gmail.com>
2023-09-07 16:21:23 +0100

    example of arrays

--- JavaScript/15.5-Collections-Working-with-JS-arrays/index.js
+++ JavaScript/15.5-Collections-Working-with-JS-arrays/index.js
@@ -1,3 +1,11 @@
  var guestList = ["Angela, "Jack, "Pam", "James", "Lara", "Jason"];
 
+var guestName = prompt("What is your name?")
+
  console.log(guestList[o]); // Angela
+
+ if (guestList.includes(guestName)) {
+    alert("Welcome!");
+ } else {
+    alert("Sorry, maybe next time!");
+ }
